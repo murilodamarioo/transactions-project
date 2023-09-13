7acca7194ac0c86409ea0dc2614eaec6ee9d0460
Murilo Damario <murilo.gcdamario@gmail.com>
2023-09-12 23:26:53 -0300

    First commit

---
+++ src/main/java/com/example/transactionsproject/TransactionsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transactionsproject/TransactionsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
