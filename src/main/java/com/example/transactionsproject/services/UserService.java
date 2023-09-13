4c3ab94ae22be40b436cbd432d3ddfe91f3df420
Murilo Damario <murilo.gcdamario@gmail.com>
2023-09-13 11:36:02 -0300

    Cria UserService

---
+++ src/main/java/com/example/transactionsproject/services/UserService.java
@@ -0,0 +1,34 @@
+package com.example.transactionsproject.services;
+
+import com.example.transactionsproject.domain.user.User;
+import com.example.transactionsproject.domain.user.UserType;
+import com.example.transactionsproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo lojista não está autorizado a realizar transação");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente para completar a transação");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+}
